cbd4b3d42729a4b2a082731c71c2a9e5326b3195
Fran Lopez <franciscolopto@gmail.com>
2023-09-11 19:42:31 +0200

    first commit

---
+++ 01-vue/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+const name = "Vue dinámico";
+const styleColor = "color: blue";
+const active = false;
+</script>
+
+<template>
+  <h1>Hola {{ name.toLocaleUpperCase() }}!</h1>
+  <h2 v-bind:style="styleColor">soy azul</h2>
+  <h2 v-if="active">v-if en acción</h2>
+  <h2 v-else>v-else en acción</h2>
+</template>
+
+<style>
+h1 {
+  color: aquamarine;
+}
+</style>
