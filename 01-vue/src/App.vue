92ef643d066c08a770470bdc0a42252bb135231d
Fran Lopez <franciscolopto@gmail.com>
2023-09-12 21:33:27 +0200

    add v-for

--- 01-vue/src/App.vue
+++ 01-vue/src/App.vue
@@ -2,6 +2,7 @@
 const name = "Vue dinámico";
 const styleColor = "color: blue";
 const active = false;
+const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
 </script>
 
 <template>
@@ -9,6 +10,12 @@ const active = false;
   <h2 v-bind:style="styleColor">soy azul</h2>
   <h2 v-if="active">v-if en acción</h2>
   <h2 v-else>v-else en acción</h2>
+
+  <ul>
+    <li v-for="(fruta, index) in arrayFrutas" v-bind:key="index">
+      {{ fruta }}
+    </li>
+  </ul>
 </template>
 
 <style>
