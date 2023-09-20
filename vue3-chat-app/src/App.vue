b102708d747c852f374eb0eeb58e63568da84f5e
Fran Lopez <franciscolopto@gmail.com>
2023-09-20 20:55:31 +0200

    initial commit

---
+++ vue3-chat-app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <h1 class="text-center mt-5 text-h1">Chat app con vuetify 3</h1>
+        <v-btn color="blue-grey darken-4" variant="outlined" prepend-icon="mdi-google-downasaur"
+          >Click aquí</v-btn
+        >
+        <v-btn class="ml-5" color="red" append-icon="mdi-google-controller">Click aquí</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
