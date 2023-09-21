36d49e4727efa98894b585892cce0d7615ca9524
Fran pc bichin <franciscolopto@gmail.com>
2023-09-21 20:22:29 +0200

    add chat component

---
+++ vue3-chat-app/src/components/ChatComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1 class="text-center">Chats</h1>
+    <v-card 
+    width="300" 
+    subtitle="Yo" 
+    text="Mensaje estático" 
+    class="mb-3 ml-auto"
+    color="success"
+    theme="dark">
+    </v-card>
+    <v-card 
+    width="300" 
+    subtitle="Noe" 
+    text="Mensaje estático" 
+    class="mb-3"
+    color="#1f7087"
+    theme="dark">
+    </v-card>
+    <v-card 
+    width="300" 
+    subtitle="Noe" 
+    text="Mensaje estático" 
+    class="mb-3"
+    color="#1f7087"
+    theme="dark">
+    </v-card>
+</template>
