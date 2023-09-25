c1a4e8bb3f0e5f0483f431671c62a6f94ca389b6
Fran Lopez <franciscolopto@gmail.com>
2023-09-25 18:42:35 +0200

    add user authentication

--- vue3-chat-app/src/App.vue
+++ vue3-chat-app/src/App.vue
@@ -1,23 +1,53 @@
 <template>
   <v-app>
-    <v-app-bar color="primary">
+    <div v-if="userGoogle === false">Loading...</div>
+    <v-app-bar color="primary" v-if="userGoogle !== false">
       <v-app-bar-title>Vuetify Chat</v-app-bar-title>
-      <v-btn variant="outlined">Acceder</v-btn>
+      <v-btn variant="outlined" @click="googleAccess" v-if="!userGoogle">Acceder</v-btn>
+      <v-btn variant="outlined" @click="logout" v-if="userGoogle">Salir</v-btn>
     </v-app-bar>
 
-    <v-main app>
+    <v-main app v-if="userGoogle !== false">
       <v-container>
-        <ChatComponent />
+        <ChatComponent v-if="userGoogle" />
+        <div v-else>
+          <h1 class="text-center mt-15">Debes iniciar sesión</h1>
+        </div>
       </v-container>
     </v-main>
 
-    <v-footer class="d-flex flex-column" app>
+    <v-footer class="d-flex flex-column" app v-if="userGoogle !== false">
       <FormAdd />
     </v-footer>
   </v-app>
 </template>
 
 <script setup>
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth'
+import { auth } from './firebase'
+import { ref } from 'vue';
+
 import ChatComponent from './components/ChatComponent.vue'
 import FormAdd from './components/FormAdd.vue'
+
+const userGoogle = ref(false)
+
+const googleAccess = async () => {
+  try {
+    const provider = new GoogleAuthProvider()
+    const { user } = await signInWithPopup(auth, provider)
+    console.log(user)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const logout = async () => {
+  await signOut(auth)
+}
+
+onAuthStateChanged(auth, (user) => {
+  console.log(user)
+  userGoogle.value = user
+})
 </script>
