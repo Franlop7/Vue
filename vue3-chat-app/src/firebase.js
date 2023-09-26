ce6dc51f2ad25f3a6f167e1b34756523e68b5c1b
Fran Lopez <franciscolopto@gmail.com>
2023-09-26 16:06:07 +0200

    update for environment variables

--- vue3-chat-app/src/firebase.js
+++ vue3-chat-app/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA_8zndltr8t3E-NfGzru8vLswR_NFjWms',
-  authDomain: 'chatvuetify-81325.firebaseapp.com',
-  projectId: 'chatvuetify-81325',
-  storageBucket: 'chatvuetify-81325.appspot.com',
-  messagingSenderId: '93115705009',
-  appId: '1:93115705009:web:bea7c656ba21cdc1352490'
-}
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+};
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
