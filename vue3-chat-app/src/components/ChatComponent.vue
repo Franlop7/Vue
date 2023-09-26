a4084e8888482da4c5bc01f4acb4c030b8f9d2e2
Fran Lopez <franciscolopto@gmail.com>
2023-09-26 16:09:44 +0200

    add auto-scroll on new message sent

--- vue3-chat-app/src/components/ChatComponent.vue
+++ vue3-chat-app/src/components/ChatComponent.vue
@@ -1,17 +1,19 @@
 <template>
-  <h1 class="text-center">Chats</h1>
-  <v-card
-    v-for="item of messages"
-    :key="item.id"
-    width="300"
-    class="mb-3"
-    theme="dark"
-    :subtitle="item.displayName"
-    :text="item.text"
-    :class="item.uid === userChat.uid && 'ml-auto'"
-    :color="item.uid === userChat.uid ? 'success' : 'pink'"
-  >
-  </v-card>
+  <div ref="chatsRef">
+    <h1 class="text-center">Chats</h1>
+    <v-card
+      v-for="item of messages"
+      :key="item.id"
+      width="300"
+      class="mb-3"
+      theme="dark"
+      :subtitle="item.displayName"
+      :text="item.text"
+      :class="item.uid === userChat.uid && 'ml-auto'"
+      :color="item.uid === userChat.uid ? 'success' : 'pink'"
+    >
+    </v-card>
+  </div>
 </template>
 
 <script setup>
@@ -22,6 +24,7 @@ import { collection, query, onSnapshot, orderBy } from 'firebase/firestore'
 const userChat = ref(auth.currentUser)
 
 const messages = ref([])
+const chatsRef = ref(null)
 
 const q = query(collection(db, 'chats'), orderBy('time'))
 const unsubscribe = onSnapshot(q, (snapshot) => {
@@ -32,6 +35,12 @@ const unsubscribe = onSnapshot(q, (snapshot) => {
         id: change.doc.id,
         ...change.doc.data()
       })
+      setTimeout(() => {
+        if (chatsRef.value !== null) {
+          console.log(chatsRef.value.scrollHeight)
+          window.scrollTo(0, chatsRef.value.scrollHeight)
+        }
+      }, 60)
     }
   })
 })
